6eb0e9c87610431d4bbef9d28d0f73df1cb6588e
Kamila <kamila.matuszak@list.pl>
2023-04-04 18:51:45 +0200

    Camera displays an image while switching rooms

--- unityProject/Assets/Scripts/Camera/CameraInput.cs
+++ unityProject/Assets/Scripts/Camera/CameraInput.cs
@@ -1,70 +1,141 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class CameraInput : MonoBehaviour
 {
-    public List<CinemachineVirtualCamera> camerasP1;
-    private int currentCameraIndexP1 = 0;
+    [SerializeField] private Image player1Walking;
+    [SerializeField] private List<CinemachineVirtualCamera> camerasP1;
+    [SerializeField] private List<CinemachineVirtualCamera> camerasP2;
+    [SerializeField] private int room;
+    [SerializeField] private Camera cameraP1;
+    private int _currentCameraIndexP1;
+    private int _currentCameraIndexP2;
 
-    public CinemachineVirtualCamera CurrentCamera
+    private CinemachineVirtualCamera CurrentCameraP1
     {
-        get { return camerasP1[currentCameraIndexP1]; }
-        set
+        set => SetProperCamera(camerasP1, value,1);
+    }
+
+    private CinemachineVirtualCamera CurrentCameraP2
+    {
+        set => SetProperCamera(camerasP2, value, 2);
+    }
+
+    /// <summary>
+    /// Sets proper camera for chosen player.
+    /// </summary>
+    /// <param name="player"> Used to make sure that proper current camera will be assigned</param>
+    private void SetProperCamera(List<CinemachineVirtualCamera> cameras, CinemachineVirtualCamera newCamera, int player)
+    {
+        //deactivate current camera
+        if (player == 1)
         {
-            //deactivate current camera
-            CameraSwitcher.Unregister(camerasP1[currentCameraIndexP1]);
+            CameraSwitcher.UnregisterP1(cameras[_currentCameraIndexP1]);
+        }
+        else
+        {
+            CameraSwitcher.UnregisterP2(cameras[_currentCameraIndexP2]);
+        }
 
-            //get the index of the new camera
-            int newIndex = camerasP1.IndexOf(value);
+        //get the index of the new camera
+        int newIndex = cameras.IndexOf(newCamera);
 
-            //check is the camera is falid
-            if (newIndex >= 0 && newIndex < camerasP1.Count)
+        //check is the camera is valid
+        if (newIndex >= 0 && newIndex < cameras.Count)
+        {
+            //set the new camera index and activate the new camera
+            if (player == 1)
             {
-                //set the new camera index and activate the new camera
-                currentCameraIndexP1 = newIndex;
-                CameraSwitcher.Register(camerasP1[newIndex]);
+                _currentCameraIndexP1 = newIndex;
+                CameraSwitcher.RegisterP1(cameras[newIndex]);
+                
             }
             else
             {
-                Debug.Log("invalif camera selection");
+                _currentCameraIndexP2 = newIndex;
+                CameraSwitcher.RegisterP2(cameras[newIndex]);
             }
+            
+        }
+        else
+        {
+            Debug.Log("Invalid camera selection");
         }
     }
 
     private void Start()
     {
-        CinemachineVirtualCamera[] allCameras = FindObjectsOfType<CinemachineVirtualCamera>();
-
-        //filter the cameras by label "Player1" and add them to the list
-        foreach (CinemachineVirtualCamera camera in allCameras)
-        {
-            if (camera.gameObject.CompareTag("Player1"))
-            {
-                camerasP1.Add(camera);
-            }
-        }
-
         //activate the 1st camera in the list
         if (camerasP1.Count > 0)
         {
-            CurrentCamera = camerasP1[currentCameraIndexP1];
+            CurrentCameraP1 = camerasP1[_currentCameraIndexP1];
         }
+
+        if (camerasP2.Count > 0)
+        {
+            CurrentCameraP2 = camerasP2[_currentCameraIndexP2];
+        }
+
+        player1Walking.enabled = false;
     }
 
     void Update()
     {
         // Check for keyboard input to cycle through the cameras
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        // if (Input.GetKeyUp(KeyCode.D))
+        // {
+        //     CurrentCameraP2 = camerasP2[(_currentCameraIndexP2 + 1) % camerasP2.Count];
+        // }
+        // else if (Input.GetKeyUp(KeyCode.A))
+        // {
+        //     // Set the current camera to the previous camera in the list
+        //     CurrentCameraP2 = camerasP2[(_currentCameraIndexP2 - 1 + camerasP2.Count) % camerasP2.Count];
+        // }
+        // if (Input.GetKeyUp(KeyCode.RightArrow))
+        // {
+        //     // Set the current camera to the next camera in the list
+        //     CurrentCameraP1 = camerasP1[(_currentCameraIndexP1 + 1) % camerasP1.Count];
+        // }
+        // else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        // {
+        //     // Set the current camera to the previous camera in the list
+        //     CurrentCameraP1 = camerasP1[(_currentCameraIndexP1 - 1 + camerasP1.Count) % camerasP1.Count];
+        // }
+
+        if (Input.GetKeyUp(KeyCode.Alpha1))
         {
-            // Set the current camera to the next camera in the list
-            CurrentCamera = camerasP1[(currentCameraIndexP1 + 1) % camerasP1.Count];
+            CurrentCameraP1 = camerasP1[1*room+0];
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
         {
-            // Set the current camera to the previous camera in the list
-            CurrentCamera = camerasP1[(currentCameraIndexP1 - 1 + camerasP1.Count) % camerasP1.Count];
+            CurrentCameraP1 = camerasP1[1*room+1];
         }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            CurrentCameraP1 = camerasP1[1*room+2];
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            CurrentCameraP1 = camerasP1[1*room+3];
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            player1Walking.enabled = true;
+            Invoke(nameof(ShowImage), 2f);
+            CurrentCameraP1 = camerasP1[1*room+4];
+        }  
+        
+
+
+    }
+
+    private void ShowImage()
+    {
+        player1Walking.enabled = false;
     }
 }
